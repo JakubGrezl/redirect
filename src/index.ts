c3bf55c5d4bb293e203c4770b2e3efe2dd062264
Jakub.Grezl <jakub.grezl@outlook.cz>
2025-05-06 17:14:09 +0200

    change-address

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export default {
-	async fetch(request): Promise<Response> {
-		const destinationURL = "https://example.com";
-		const statusCode = 301;
-		return Response.redirect(destinationURL, statusCode);
-	},
+  async fetch(request): Promise<Response> {
+    const destinationURL =
+      "https://poised-jupiter-7ea.notion.site/Maturita-196742d468a880a493bad7ec858c269f?pvs=74";
+    const statusCode = 301;
+    return Response.redirect(destinationURL, statusCode);
+  },
 } satisfies ExportedHandler;
